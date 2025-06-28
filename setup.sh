ea90ca26b540085d1e6538053dad5a5c29a3468b
Arthur Braga <arthurbraga682@gmail.com>
2025-06-28 17:14:18 -0300

    Release Habits! MVP

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# Install Homebrew if you missed it
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install git gh            # git & GitHub CLI
+python3 --version              # ≥ 3.9?
+python3 -m pip install --upgrade pip virtualenv
